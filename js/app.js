94918fbb494dd3fa8c9e0f1b3a3b6b317ed466d2
Emma van Wincoop <EWI@afasgroep.nl>
2026-06-15 15:52:58 +0200

    Overheid & Onderwijs thema: rood/blauw kleuren

--- js/app.js
+++ js/app.js
@@ -227,7 +227,7 @@ function startConfetti() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const colors = ['#00a651', '#f0c040', '#ff6b6b', '#4ecdc4', '#6c5ce7', '#fff'];
+    const colors = ['#c8102e', '#005baa', '#ffffff', '#e8314b', '#3d8edb', '#f0c040'];
     const particles = [];
     for (let i = 0; i < 150; i++) {
         particles.push({
